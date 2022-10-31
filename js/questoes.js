2c8bd31d64aaa864e3de6ccbdb2ff0407e31586c
Davi Varella <110793193+Davizukss@users.noreply.github.com>
2022-10-31 14:17:16 -0300

    Add files via upload

--- js/questoes.js
+++ js/questoes.js
@@ -1,63 +1,63 @@
 let questoes =[
     {
         ordem: 1,
-        questao: "Qualquer pergunta aqui?",
-        resposta: "Resposta 3",
+        questao: " O que é administração?",
+        resposta: "Gerenciamento de pessoas e recursos",
         opcoes:[
-            "te minus, tenetur libero! Veniam, architecto.",
-            "Resposta 2",
-            "Resposta 3",
-            "Resposta 4",
-            "Resposta 5"
+            "Gerenciamento social organizado",
+            "Gerenciamento de pessoas e recursos",
+            "Gerenciamento de serviços e produção",
+            "Organização de pessoas e sociedade",
+            "Organização de prejuízo e ganho de uma empresa"
         ]
 
     },
     {
         ordem: 2,
-        questao: "Qualquer pergunta aqui?",
-        resposta: "Resposta 1",
+        questao: "Qual desses NÃO faz parte do setor de logística?",
+        resposta: "Logística de aproveitamento",
         opcoes:[
-            "Resposta 1",
-            "Resposta 2",
-            "Resposta 3",
-            "Resposta 4",
-            "Resposta 5"
+            "Logística de produção.",
+            "Logística de distribuição.",
+            "Logística de aproveitamento",
+            "Logística de suprimentos.",
+            "Logística reversa."
         ]
     },
     {
         ordem: 3,
-        questao: "Qualquer pergunta aqui?",
-        resposta: "Resposta 2",
+        questao: "Economia é de qual área de estudo?",
+        resposta: "Humanas",
         opcoes:[
-            "Resposta 1",
-            "Resposta 2",
-            "Resposta 3",
-            "Resposta 4",
-            "Resposta 5"
+            "Humanas",
+            "Social",
+            "Biológicas",
+            "Exatas",
+            "Ambos"
         ]
     },
     {
         ordem: 4,
-        questao: "Qualquer pergunta aqui?",
-        resposta: "Resposta 5",
+        questao: "O que o setor de NÃO marketing faz?",
+        resposta: "Gerencias vendas e preferências do público",
         opcoes:[
-            "Resposta 1",
-            "Resposta 2",
-            "Resposta 3",
-            "Resposta 4",
-            "Resposta 5"
+            "Planejamento da comunicação da empresa",
+            "Gerencias vendas e preferências do público",
+            "Desenvolver e gerenciar campanhas promocionais",
+            "Gerenciar produção multimídia",
+            "Analisar e mensurar resultados de campanhas"
         ]
     },
     {
         ordem: 5,
-        questao: "Qualquer pergunta aqui?",
-        resposta: "Resposta 4",
+        questao: "Qual dessas áreas é responsável por gerenciar a distribuição e produção de produtos?",
+        resposta: "Logística",
         opcoes:[
-            "Resposta 1",
-            "Resposta 2",
-            "Resposta 3",
-            "Resposta 4",
-            "Resposta 5"
+            " Administração",
+            "Logística",
+            "Economia",
+            "Marketing",
+            "Nenhuma das anteriores"
         ]
     },
 
